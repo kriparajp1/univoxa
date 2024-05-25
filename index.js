b3bed389994c528023a94b6664af71432ac67ccf
kriparaj P <156789243+kriparajp1@users.noreply.github.com>
2024-05-25 13:01:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ io.on('connection', (socket) => {
 
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg); 
-  });
+  }); 
 
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
-});
-
+}); 
+ 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
